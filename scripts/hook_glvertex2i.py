cf221fe56ebea7b919b189ba8ae452e03447cf78
Nicolas Suarez <ns5732@nyu.edu>
2025-07-23 22:49:44 -0400

    add working screen res pull

--- scripts/hook_glvertex2i.py
+++ scripts/hook_glvertex2i.py
@@ -2,9 +2,17 @@ import frida
 import sys
 import os
 from datetime import datetime
+import ctypes
 
 # Write messages to file
 with open("frida_hook_dump.txt", "w") as f:
+
+    # Get screen size
+    user32 = ctypes.windll.user32
+    screen_width = user32.GetSystemMetrics(0)
+    screen_height = user32.GetSystemMetrics(1)
+
+    print(str(screen_width) + "x" + str(screen_height))
 
     # Handle messages from Frida
     def on_message(message, data):
@@ -21,7 +29,19 @@ with open("frida_hook_dump.txt", "w") as f:
     session = frida.attach(pid)
 
     # Frida script
-    script = session.create_script("""                       
+    script = session.create_script("""
+
+    // Get variables from python
+    function listen(){
+        recv(function(message){
+            var screen_width = message.payload.screen_width;
+            var screen_height = message.payload.screen_height;
+                                   
+            globalThis.screenSettings = { screen_width, screen_height };
+        })
+    }                           
+    listen();
+                                   
     // Print all calls to glVertex2i
     Interceptor.attach(Module.getGlobalExportByName("glVertex2i"), {
         onEnter(args) {
@@ -29,30 +49,24 @@ with open("frida_hook_dump.txt", "w") as f:
             var x = args[0].toInt32();
             var y = args[1].toInt32();
                                    
-            var black_bar_height_top = 64;
-            var black_bar_height_bottom = 31;
+            var black_bar_height_top = 424;
+            var black_bar_height_bottom = 391;
                                    
-            var additional_width = 537;
+            var width_modification = 102;
                                    
-            if(x == 0 && y == 704){
-                // args[1] = ptr(1080 - black_bar_height_top);
-                args[1] = ptr(1440);
+            if(x == 0 && y == 704){             // (0,0)
+                args[1] = ptr(globalThis.screenSettings.screen_height - black_bar_height_top);
             }
-            else if(x == 1312 && y == 704){
-                // args[0] = ptr(1920 + additional_width);
-                // args[1] = ptr(1080 - black_bar_height_top);
-                args[0] = ptr(2560);
-                args[1] = ptr(1440);
+            else if(x == 1312 && y == 704){     // (1,0)
+                args[0] = ptr(globalThis.screenSettings.screen_width - width_modification);
+                args[1] = ptr(globalThis.screenSettings.screen_height - black_bar_height_top);
             }
-            else if(x == 1312 && y == -685){
-                // args[0] = ptr(1920 + additional_width);
-                // args[1] = ptr(-1080 + black_bar_height_bottom);
-                args[0] = ptr(2560);
-                args[1] = ptr(-1440);
+            else if(x == 1312 && y == -685){    // (1,1)
+                args[0] = ptr(globalThis.screenSettings.screen_width - width_modification);
+                args[1] = ptr(-globalThis.screenSettings.screen_height + black_bar_height_bottom);
             }
-            else if(x == 0 && y == -685){
-                // args[1] = ptr(-1080 + black_bar_height_bottom);
-                args[1] = ptr(-1440);
+            else if(x == 0 && y == -685){       // (0,1)
+                args[1] = ptr(-globalThis.screenSettings.screen_height + black_bar_height_bottom);
             }
             else if(x == 0 && y == -662){
                 args[1] = ptr(-1080);
@@ -71,6 +85,16 @@ with open("frida_hook_dump.txt", "w") as f:
 
     script.on('message', on_message)
     script.load()
+
+    # Send variables to Frida script
+    script.post({
+        "type": "data",
+        "payload": {
+          "screen_width": screen_width,
+          "screen_height": screen_height
+        }
+    })
+
 
     # Start the process
     frida.resume(pid)
